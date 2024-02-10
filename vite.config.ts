778bedba0c0f9c1f17102f747e9409c0f97af54f
Сергей Сушков <sushkov_sv@ursip.ru>
2024-02-10 14:13:52 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -18,10 +18,11 @@ export default defineConfig({
     }),
   ],
   // TODO: Если всё работает, удалить.
-  define: {
-    'process.env': {},
-    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL),
-  },
+  // С этим кодом не работает import.meta.env...
+  // define: {
+  //   'process.env': {},
+  //   'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL),
+  // },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
